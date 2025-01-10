583e2758eedcd1461fcd8476631d9799a2c076d7
md-liton <mdliton5506@gmail.com>
2025-01-10 13:13:09 +0600

    Small-Device-Done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 let menuIcon = document.querySelector('.menu_icon')
 let bar = document.querySelector('.bar')
 let close = document.querySelector('.close')
+let menu = document.querySelector('.menu')
+let login = document.querySelector('.login')
+
 
 
 menuIcon.addEventListener('click', function () {
     bar.classList.toggle('menu_active');
     close.classList.toggle('menu_active');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none'; 
+        login.style.display = 'none'; 
+    } else {
+        menu.style.display = 'block'; 
+        login.style.display = 'block'; 
+    }
 });
